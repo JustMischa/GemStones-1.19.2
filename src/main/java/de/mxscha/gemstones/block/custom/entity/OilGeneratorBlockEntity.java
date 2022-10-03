bde81356eb2b091a003dadd3a9bcae352324f87b
JustMischa <michael.geywitz@gmail.com>
2022-10-03 19:16:32 +0200

    added smelting recipes

--- src/main/java/de/mxscha/gemstones/block/custom/entity/OilGeneratorBlockEntity.java
+++ src/main/java/de/mxscha/gemstones/block/custom/entity/OilGeneratorBlockEntity.java
@@ -14,22 +14,17 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.Nullable;
-
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class OilGeneratorBlockEntity extends BlockEntity implements MenuProvider {
@@ -44,7 +39,7 @@ public class OilGeneratorBlockEntity extends BlockEntity implements MenuProvider
     private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
     protected final ContainerData data;
     private int progress = 0;
-    private int maxProgress = 78;
+    private int maxProgress = 200;
 
     public OilGeneratorBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.OIL_GENERATOR.get(), pos, state);
@@ -153,9 +148,9 @@ public class OilGeneratorBlockEntity extends BlockEntity implements MenuProvider
         Level level = entity.level;
         SimpleContainer inventory = new SimpleContainer(entity.itemHandler.getSlots());
         Optional<OilGeneratorRecipe> match = level.getRecipeManager().getRecipeFor(OilGeneratorRecipe.Type.INSTANCE, inventory, level);
-        if(match.isPresent()) {
+        if (hasRecipe(entity)) {
             entity.itemHandler.extractItem(0, 1, false);
-            entity.itemHandler.insertItem(1, new ItemStack(match.get().getResultItem().getItem()), false);
+            entity.itemHandler.setStackInSlot(1, new ItemStack(ModItems.OIL_BUCKET.get()));
         }
     }
 
